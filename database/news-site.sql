937b352a9b9e4d34797a4cfcd76986c876e68d20
RAJANIKANTA BISWAL <rajanikantabiswal15@gmail.com>
2023-10-05 18:07:33 +0530

    Update news-site.sql

--- database/news-site.sql
+++ database/news-site.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2023 at 02:26 PM
+-- Generation Time: Oct 05, 2023 at 02:36 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -38,9 +38,8 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`category_id`, `category_name`, `post`) VALUES
-(62, 'category 3', 0),
-(61, 'Category 1', 0),
-(63, 'category 4', 0);
+(64, 'Gspeed', 2),
+(61, 'Eureka Learnings', 5);
 
 -- --------------------------------------------------------
 
@@ -57,6 +56,19 @@ CREATE TABLE `post` (
   `author` int(11) NOT NULL,
   `post_img` varchar(100) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`post_id`, `title`, `description`, `category`, `post_date`, `author`, `post_img`) VALUES
+(59, 'Post 1 for GSPEED', 'GS desc', '64', '03 Oct, 2023', 52, 'logo (2).png'),
+(61, 'A post on Eureka Learnings', 'Eureka Learnings is a best company in bhubaneswar for recruiting', '61', '03 Oct, 2023', 54, 'eurekalearnings-logo.png'),
+(62, 'Third post on GSpeed', 'gspeed third post', '64', '03 Oct, 2023', 54, 'mindfire.png'),
+(63, '4th post on eureka learnings', 'eureka learnings 4th post description', '61', '03 Oct, 2023', 54, 'Polycab.jpg'),
+(64, '4th post for eureka learnings', 'eureka learnings desc', '61', '03 Oct, 2023', 52, 'Process Map.png'),
+(65, 'eureka learnings 5th post', '5th desc', '61', '03 Oct, 2023', 52, 'react.png'),
+(66, 'New post 27', 'description for new post 27', '61', '05 Oct, 2023', 52, 'silhouettes-of-business-people-in-modern-office-interior.jpg');
 
 -- --------------------------------------------------------
 
@@ -79,7 +91,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `username`, `password`, `role`) VALUES
 (52, 'Rajanikanta', 'Biswal', 'admin', '5f4dcc3b5aa765d61d8327deb882cf99', 1),
-(53, 'Urmi mala', 'Swain', 'urmi', '5f4dcc3b5aa765d61d8327deb882cf99', 0);
+(54, 'Urmi Mala', 'Swain', 'urmi', '5f4dcc3b5aa765d61d8327deb882cf99', 0);
 
 --
 -- Indexes for dumped tables
@@ -112,19 +124,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
